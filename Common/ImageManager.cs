55076831ba090e5c6a9b458c3b154dadf8dea8d8
一 龍桜(にのまえ りお) <58287542+ario1281@users.noreply.github.com>
2025-12-09 15:58:38 +0900

    Update ImageManager.cs

--- Common/ImageManager.cs
+++ Common/ImageManager.cs
@@ -95,46 +95,6 @@ namespace ImageMerge.Common
             return rawFiles.Any();
         }
 
-        public static Bitmap DrawImage(List<RawFile> rawFiles)
-        {
-            var result = new Bitmap(1, 1, PixelFormat.Format32bppArgb);
-
-            if (rawFiles != null && rawFiles.Count > 0)
-            {
-                var img = rawFiles[0].image;
-                var size = img != null ? new Size(img.Width, img.Height) : new Size(1, 1);
-
-                result = new Bitmap(size.Width, size.Height, PixelFormat.Format32bppArgb);
-
-                foreach (var rawFile in rawFiles)
-                {
-                    result = MergeImage(result, rawFile.image);
-                }
-            }
-
-            return result;
-        }
-
-        public static void SaveImage(Bitmap img, string outDir, IProgress<int> progress = null)
-        {
-            Directory.CreateDirectory(outDir);
-
-            string baseName = "output";
-            string ext = ".png";
-            string filePath = Path.Combine(outDir, baseName + ext);
-
-            int cnt = 0;
-
-            do
-            {
-                filePath = Path.Combine(outDir, $"{baseName}_{cnt:000}{ext}");
-                cnt++;
-            }
-            while (File.Exists(filePath));
-
-            img.Save(filePath, ImageFormat.Png);
-        }
-
         /// <summary>
         /// 2枚の画像を1枚に合成する（高品質変換）。重ね順の反転可能。<br/>
         /// 大きさが異なる場合、高幅それぞれ大きい方に合わせる。
@@ -143,7 +103,7 @@ namespace ImageMerge.Common
         /// <param name="_in2">入力画像2（通常:背面）</param>
         /// <param name="isInv">画像の重ね順を反転</param>
         /// <returns>合成画像</returns>
-        private static Bitmap MergeImage(Bitmap _in1, Bitmap _in2, bool isInv = false)
+        public static Bitmap MergeImage(Bitmap _in1, Bitmap _in2, bool isInv = false)
         {
             if (_in1 == null || _in2 == null)
             {
@@ -188,7 +148,7 @@ namespace ImageMerge.Common
         /// <param name="_in">入力画像</param>
         /// <param name="scale">拡縮率</param>
         /// <returns>変更後の画像</returns>
-        private static Bitmap ScaleImage(Bitmap _in, float scale)
+        public static Bitmap ScaleImage(Bitmap _in, float scale)
         {
             if (_in == null)
             {
@@ -226,7 +186,7 @@ namespace ImageMerge.Common
         /// <param name="wScale">Width 拡縮率</param>
         /// <param name="hScale">Height 拡縮率</param>
         /// <returns>変更後の画像</returns>
-        private static Bitmap ScaleImage(Bitmap _in, float wScale, float hScale)
+        public static Bitmap ScaleImage(Bitmap _in, float wScale, float hScale)
         {
             if (_in == null)
             {
@@ -263,7 +223,7 @@ namespace ImageMerge.Common
         /// <param name="_in">入力画像</param>
         /// <param name="scale">Width/Height 拡縮率</param>
         /// <returns>変更後の画像</returns>
-        private static Bitmap ScaleImage(Bitmap _in, SizeF scale)
+        public static Bitmap ScaleImage(Bitmap _in, SizeF scale)
         {
             return ScaleImage(_in, scale.Width, scale.Height);
         }
@@ -274,7 +234,7 @@ namespace ImageMerge.Common
         /// <param name="_in">入力画像</param>
         /// <param name="opacity">不透明度</param>
         /// <returns>変更後の画像</returns>
-        private static Bitmap OpacityImage(Bitmap _in, float opacity)
+        public static Bitmap OpacityImage(Bitmap _in, float opacity)
         {
             if (_in == null)
             {
