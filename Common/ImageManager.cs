66e4d1cca57735b20f879f4c69bb2677fd77baaa
一 龍桜(にのまえ りお) <58287542+ario1281@users.noreply.github.com>
2025-12-09 14:22:58 +0900

    Update ImageManager.cs

--- Common/ImageManager.cs
+++ Common/ImageManager.cs
@@ -126,13 +126,13 @@ namespace ImageMerge.Common
         }
 
         /// <summary>
-        /// 2枚の画像を1枚に合成する。重ね順の反転可能。<br/>
+        /// 2枚の画像を1枚に合成する（高品質変換）。重ね順の反転可能。<br/>
         /// 大きさが異なる場合、高幅それぞれ大きい方に合わせる。
         /// </summary>
         /// <param name="_in1">入力画像1（通常:前面）</param>
         /// <param name="_in2">入力画像2（通常:背面）</param>
         /// <param name="isInv">画像の重ね順を反転</param>
-        /// <returns>合成画像（Bitmap）</returns>
+        /// <returns>合成画像</returns>
         private static Bitmap MergeImage(Bitmap _in1, Bitmap _in2, bool isInv = false)
         {
             if (_in1 == null || _in2 == null)
@@ -173,11 +173,11 @@ namespace ImageMerge.Common
         }
 
         /// <summary>
-        /// 画像を指定の倍率で拡縮します（高品質変換）。
+        /// 画像を指定の倍率で拡縮する（高品質変換）。
         /// </summary>
         /// <param name="_in">入力画像</param>
         /// <param name="scale">拡縮率</param>
-        /// <returns>リサイズ後画像</returns>
+        /// <returns>変更後の画像</returns>
         private static Bitmap ScaleImage(Bitmap _in, float scale)
         {
             if (_in == null)
@@ -211,12 +211,12 @@ namespace ImageMerge.Common
         }
 
         /// <summary>
-        /// 画像を指定の不透明度で出力します（高品質変換）。
+        /// 画像を指定の不透明度(α値)で出力する（高品質変換）。
         /// </summary>
         /// <param name="_in">入力画像</param>
         /// <param name="opacity">不透明度</param>
-        /// <returns>リサイズ後画像</returns>
-        private static Bitmap OpacityImage(Image _in, float opacity)
+        /// <returns>変更後の画像</returns>
+        private static Bitmap OpacityImage(Bitmap _in, float opacity)
         {
             if (_in == null)
             {
